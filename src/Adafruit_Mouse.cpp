499b841958bd0b66a54d7a96a51a65fb0fe9f3b6
ladyada <limor@ladyada.net>
2016-10-03 00:38:56 -0400

    absolutely!

--- src/Adafruit_Mouse.cpp
+++ src/Adafruit_Mouse.cpp
@@ -19,19 +19,22 @@
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-#include "Mouse.h"
+#include "Adafruit_Mouse.h"
+
+#define HID_REPORTID_MOUSE     (1)
+#define HID_REPORTID_MOUSE_ABS (5)
 
 #if defined(_USING_HID)
 
 static const uint8_t _hidReportDescriptor[] PROGMEM = {
-  
-  //  Mouse
-    0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)  // 54
+
+    //  Mouse relative
+    0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)	// 54
     0x09, 0x02,                    // USAGE (Mouse)
     0xa1, 0x01,                    // COLLECTION (Application)
     0x09, 0x01,                    //   USAGE (Pointer)
     0xa1, 0x00,                    //   COLLECTION (Physical)
-    0x85, 0x01,                    //     REPORT_ID (1)
+    0x85, HID_REPORTID_MOUSE,      //     REPORT_ID (1)
     0x05, 0x09,                    //     USAGE_PAGE (Button)
     0x19, 0x01,                    //     USAGE_MINIMUM (Button 1)
     0x29, 0x03,                    //     USAGE_MAXIMUM (Button 3)
@@ -54,27 +57,58 @@ static const uint8_t _hidReportDescriptor[] PROGMEM = {
     0x81, 0x06,                    //     INPUT (Data,Var,Rel)
     0xc0,                          //   END_COLLECTION
     0xc0,                          // END_COLLECTION
+
+    //  Mouse absolute
+    0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
+    0x09, 0x02,                    // USAGE (Mouse)
+    0xa1, 0x01,                    // COLLECTION (Application)
+    0x09, 0x01,                    //   USAGE (Pointer)
+    0xa1, 0x00,                    //   COLLECTION (Physical)
+    0x85, HID_REPORTID_MOUSE_ABS,  //     REPORT_ID (5)
+    0x05, 0x09,                    //     USAGE_PAGE (Button)
+    0x19, 0x01,                    //     USAGE_MINIMUM (Button 1)
+    0x29, 0x03,                    //     USAGE_MAXIMUM (Button 3)
+    0x15, 0x00,                    //     LOGICAL_MINIMUM (0)
+    0x25, 0x01,                    //     LOGICAL_MAXIMUM (1)
+    0x95, 0x03,                    //     REPORT_COUNT (3)
+    0x75, 0x01,                    //     REPORT_SIZE (1)
+    0x81, 0x02,                    //     INPUT (Data,Var,Abs)
+    0x95, 0x01,                    //     REPORT_COUNT (1)
+    0x75, 0x05,                    //     REPORT_SIZE (5)
+    0x81, 0x03,                    //     INPUT (Cnst,Var,Abs)
+    0x05, 0x01,                    //     USAGE_PAGE (Generic Desktop)
+    0x09, 0x30,                    //     USAGE (X)
+    0x09, 0x31,                    //     USAGE (Y)
+    0x09, 0x38,                    //     USAGE (Wheel)
+    0x16, 0x00, 0x80,              //     LOGICAL_MINIMUM (-32768)
+    0x26, 0xff, 0x7f,              //     LOGICAL_MAXIMUM (32767)
+    0x75, 0x10,                    //     REPORT_SIZE (16)
+    0x95, 0x03,                    //     REPORT_COUNT (3)
+    0x81, 0x02,                    //     INPUT (Data,Var,Abs)
+    0xc0,                          //   END_COLLECTION
+    0xc0,                          // END_COLLECTION
 };
+
 
 //================================================================================
 //================================================================================
 //	Mouse
 
-Mouse_::Mouse_(void) : _buttons(0)
+Adafruit_Mouse::Adafruit_Mouse(void) : _buttons(0)
 {
-    static HIDSubDescriptor node(_hidReportDescriptor, sizeof(_hidReportDescriptor));
-    HID().AppendDescriptor(&node);
+   static HIDSubDescriptor node(_hidReportDescriptor, sizeof(_hidReportDescriptor));
+   HID().AppendDescriptor(&node);
 }
 
-void Mouse_::begin(void) 
-{
-}
-
-void Mouse_::end(void) 
+void Adafruit_Mouse::begin(void) 
 {
 }
 
-void Mouse_::click(uint8_t b)
+void Adafruit_Mouse::end(void) 
+{
+}
+
+void Adafruit_Mouse::click(uint8_t b)
 {
 	_buttons = b;
 	move(0,0,0);
@@ -82,7 +116,7 @@ void Mouse_::click(uint8_t b)
 	move(0,0,0);
 }
 
-void Mouse_::move(signed char x, signed char y, signed char wheel)
+void Adafruit_Mouse::move(signed char x, signed char y, signed char wheel)
 {
 	uint8_t m[4];
 	m[0] = _buttons;
@@ -92,7 +126,27 @@ void Mouse_::move(signed char x, signed char y, signed char wheel)
 	HID().SendReport(1,m,4);
 }
 
-void Mouse_::buttons(uint8_t b)
+// all parameters have the range of -32768 to 32767 and must be scaled to screen pixels
+// some examples:
+//   x=0,y=0 is the middle of the screen
+//   x=-32768,y=-32768 is the top left corner
+//   x=32767,y=-32768 is the top right corner
+//   x=32767,y=32767 is the bottom right corner
+//   x=-32768,y=32767 is the bottom left corner
+void Adafruit_Mouse::moveAbs(int16_t x, int16_t y, int16_t wheel)
+{
+	u8 m[7];
+	m[0] = _buttons; // TODO: is it a good idea to take over the _buttons from relative report here or should it be left out?
+	m[1] = x & 0xFF;
+	m[2] = x >> 8;
+	m[3] = y & 0xFF;
+	m[4] = y >> 8;
+	m[5] = wheel & 0xFF;
+	m[6] = wheel >> 8;
+	HID().SendReport(HID_REPORTID_MOUSE_ABS,m,sizeof(m));
+}
+
+void Adafruit_Mouse::buttons(uint8_t b)
 {
 	if (b != _buttons)
 	{
@@ -101,23 +155,21 @@ void Mouse_::buttons(uint8_t b)
 	}
 }
 
-void Mouse_::press(uint8_t b) 
+void Adafruit_Mouse::press(uint8_t b) 
 {
 	buttons(_buttons | b);
 }
 
-void Mouse_::release(uint8_t b)
+void Adafruit_Mouse::release(uint8_t b)
 {
 	buttons(_buttons & ~b);
 }
 
-bool Mouse_::isPressed(uint8_t b)
+bool Adafruit_Mouse::isPressed(uint8_t b)
 {
 	if ((b & _buttons) > 0) 
 		return true;
 	return false;
 }
-
-Mouse_ Mouse;
 
 #endif
--- src/Adafruit_Mouse.h
+++ src/Adafruit_Mouse.h
@@ -19,8 +19,8 @@
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-#ifndef MOUSE_h
-#define MOUSE_h
+#ifndef ADAFRUIT_MOUSE_h
+#define ADAFRUIT_MOUSE_h
 
 #include "HID.h"
 
@@ -39,22 +39,23 @@
 #define MOUSE_MIDDLE 4
 #define MOUSE_ALL (MOUSE_LEFT | MOUSE_RIGHT | MOUSE_MIDDLE)
 
-class Mouse_
+class Adafruit_Mouse
 {
 private:
   uint8_t _buttons;
   void buttons(uint8_t b);
 public:
-  Mouse_(void);
+  Adafruit_Mouse(void);
   void begin(void);
   void end(void);
   void click(uint8_t b = MOUSE_LEFT);
   void move(signed char x, signed char y, signed char wheel = 0); 
+  void moveAbs(int16_t x, int16_t y, int16_t wheel = 0);
+
   void press(uint8_t b = MOUSE_LEFT);   // press LEFT by default
   void release(uint8_t b = MOUSE_LEFT); // release LEFT by default
   bool isPressed(uint8_t b = MOUSE_LEFT); // check LEFT by default
 };
-extern Mouse_ Mouse;
 
 #endif
 #endif
